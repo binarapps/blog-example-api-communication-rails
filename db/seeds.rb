fe246dec656e575198d777d956350b3f00df29e9
Michał Gutowski <m.gutowski@binarapps.com>
2014-06-14 23:40:26 +0200

    small fix in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 def seed_image(file_name)
-  File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}"))
+  File.open(File.join(Rails.root, "/app/assets/images/seeds/#{file_name}"))
 end
 
 Entry.create(name: "sample entry", text: "sample text", file: seed_image("seed.jpg"))
